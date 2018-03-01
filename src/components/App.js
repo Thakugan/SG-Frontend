77590a6a202c9b2ef59d63434315a0f267fb8f79
Jenn Le <jenn.thakugan@icloud.com>
2018-03-01 16:25:47 -0600

    Added some content, testing deployment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+const Landing = () => (
+  <div>
+    <p className="App-intro">
+      The purpose of this demo site is to display working functionality of the
+      Statgeek Analytics API. To get started, pick an action from below.
+    </p>
+    <div className="row mt-5">
+      <Link to="/team" className="btn btn-dark" style={{ margin: "0 10px" }}>
+        See Team Stats
+      </Link>
+    </div>
+  </div>
+);
+const Team = () => <h2>Team</h2>;
 
 class App extends Component {
   render() {
     return (
-      <div className="container mt-5 App">
-        <header className="App-header">
-          <h1 className="App-title">Statgeek Analytics Demo</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <header className="App-header mt-5">
+              <h1 className="App-title">Statgeek Analytics Demo</h1>
+            </header>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/team" component={Team} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./components/App";
+import reducers from "./reducers";
+import registerServiceWorker from "./registerServiceWorker";
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
+
 registerServiceWorker();
