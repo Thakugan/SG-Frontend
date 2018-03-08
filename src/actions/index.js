75fcb9832116ff73c327de1d0dfe59ebd8cd9328
Jenn Le <jenn.thakugan@icloud.com>
2018-03-08 16:08:14 -0600

    team stats show up

--- src/actions/index.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const getTeamNames = () => async dispatch => {
-  const res = await axios.get("http://54.147.204.57:5000/names");
-  return res.data;
-};
--- src/components/TeamStats.js
+++ src/components/TeamStats.js
@@ -1,61 +1,58 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class TeamStats extends Component {
-  teamNames() {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      names: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://54.147.204.57:5000/names").then(res => {
+      this.setState({ names: res.data });
+    });
+  }
+
+  teamSelected = event => {
+    this.setState({ team: event.target.value });
+    axios.get("http://54.147.204.57:5000/team/" + this.state.team).then(res => {
+      this.setState({ stats: res.data });
+    });
+  };
 
   render() {
     return (
       <div>
         <h2>Team Stats</h2>
-        <p>Pick a team: </p>
-        <div class="container row">
-          <table class="table table-striped table-bordered fixed">
-            <col width="100px" />
-            <col width="100px" />
-            <tbody>
-              <tr>
-                <td>Team</td>
-                <td />
-              </tr>
-              <tr>
-                <td>League</td>
-                <td />
-              </tr>
-              <tr>
-                <td>FGM</td>
-                <td />
-              </tr>
-              <tr>
-                <td>FG3</td>
-                <td />
-              </tr>
-              <tr>
-                <td>FGA</td>
-                <td />
-              </tr>
-              <tr>
-                <td>TO</td>
-                <td />
-              </tr>
-              <tr>
-                <td>FTA</td>
-                <td />
-              </tr>
-              <tr>
-                <td>FT</td>
-                <td />
-              </tr>
-              <tr>
-                <td>ORebs</td>
-                <td />
-              </tr>
-              <tr>
-                <td>DRebs</td>
-                <td />
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <form style={{ width: "50%" }}>
+          <div className="form-group">
+            <label htmlFor="team" />
+            <select
+              className="form-control"
+              id="team"
+              onChange={this.teamSelected}
+            >
+              <option>Select a Team</option>/>
+              {this.state
+                ? this.state.names.map(function(name) {
+                    return (
+                      <option key={name} value={name}>
+                        {name}
+                      </option>
+                    );
+                  })
+                : null}
+            </select>
+          </div>
+        </form>
+
+        {this.state ? (
+          this.state.stats ? (
+            <p>{JSON.stringify(this.state.stats)}</p>
+          ) : null
+        ) : null}
       </div>
     );
   }
